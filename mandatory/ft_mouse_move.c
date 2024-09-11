295c667745e0090a00cd1a9c8612634a2c9c251d
Galaad Mersch <gmersch@1F4.42angouleme.fr>
2024-09-11 15:21:38 +0200

    switch branche, main become the updated one

--- mandatory/ft_mouse_move.c
+++ mandatory/ft_mouse_move.c
@@ -6,24 +6,25 @@
 /*   By: gmersch <gmersch@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 01:07:29 by gmersch           #+#    #+#             */
-/*   Updated: 2024/09/03 13:52:25 by gmersch          ###   ########.fr       */
+/*   Updated: 2024/09/10 18:03:23 by gmersch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube.h"
 
-void	ft_mouse_move(double x, double y, void *param)
+void	ft_mouse_move(t_player *p)
 {
-	t_player	*p;
+	int32_t	x;
+	int32_t	y;
 
-	p = (t_player *)param;
-
+	if (!p->last_mouse_x)
+		p->last_mouse_x = p->game->width / 2;
+	mlx_get_mouse_pos(p->game->mlx, &x, &y);
 	if (p->game->pause == false)
 	{
-		//p->last_mouse_x = p->game->width / 2;
 		p->or += (x - p->last_mouse_x) * 0.001;
-		mlx_set_mouse_pos(p->game->mlx, p->game->width / 2, p->game->height / 2);
-		p->last_mouse_x = p->game->width / 2; //on linux
-		//p->last_mouse_x = x;// in WSL2
+		mlx_set_mouse_pos(p->game->mlx, p->game->width / 2, \
+		p->game->height / 2);
+		p->last_mouse_x = p->game->width / 2;
 	}
 }
